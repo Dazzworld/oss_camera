7bc8152a91a2903eb64e27a49890a0c7d0c56507
chenzc <dazzworld@163.com>
2019-03-06 13:59:03 +0800

    Revert "QCamera1:HAL1:output fv and spot light detect result"
    This reverts commit 9cc1391c195d02b5dc668544b567f87acf441343.

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -1374,8 +1374,6 @@ typedef struct {
     int32_t focus_pos;
     cam_af_flush_info_t flush_info;
     uint8_t isDepth;
-    float focus_value;
-    uint8_t spot_light_detected;
 } cam_auto_focus_data_t;
 
 typedef struct {
@@ -2213,10 +2211,6 @@ typedef enum {
        current configuration for pic res*/
     CAM_INTF_META_STREAM_INFO_FOR_PIC_RES,
     CAM_INTF_META_FOCUS_DEPTH_INFO,
-    /*Focus value output from af core*/
-    CAM_INTF_META_FOCUS_VALUE,
-    /*Spot light detection result output from af core*/
-    CAM_INTF_META_SPOT_LIGHT_DETECT,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
